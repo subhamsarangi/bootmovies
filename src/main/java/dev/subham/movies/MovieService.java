bf5c3bcb4729888d753fe37bc69e779eaa562540
Subham Sarangi <subham.sarangi@ivanwebsolutions.com>
2023-05-02 19:21:23 +0530

    first

---
+++ src/main/java/dev/subham/movies/MovieService.java
@@ -0,0 +1,25 @@
+package dev.subham.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+
+//    public Optional<Movie> oneMovie(ObjectId id){
+//        return movieRepository.findById(id);
+//    }
+
+    public Optional<Movie> oneMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
---
+++ src/test/java/dev/subham/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.subham.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
